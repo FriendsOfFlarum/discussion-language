4dd809e5b4ebd315bc6b245434f96c57fda4bac1
David Wheatley <hi@davwheat.dev>
2021-08-19 17:28:22 +0200

    Hide language option in Byobu composer; hide lang on byobu discussions if somehow present; format code

--- js/src/forum/addLanguageComposer.js
+++ js/src/forum/addLanguageComposer.js
@@ -9,53 +9,62 @@ import Language from './components/Language';
 const sort = (a, b) => a.code().toLowerCase() > b.code().toLowerCase();
 
 export default () => {
-    extend(IndexPage.prototype, 'newDiscussionAction', function (promise) {
-        const dislang = app.search.params().language;
+  extend(IndexPage.prototype, 'newDiscussionAction', function (promise) {
+    const dislang = app.search.params().language;
 
-        if (dislang) {
-            promise.then((composer) => (composer.fields.language = app.store.getBy('discussion-languages', 'code', dislang)));
-        } else {
-            const localeComposer = app.forum.attribute('fof-discussion-language.composerLocaleDefault');
-            app.composer.fields.language = localeComposer ? app.store.getBy('discussion-languages', 'code', app.translator.formatter.locale) : '';
-        }
+    if (dislang) {
+      promise.then((composer) => (composer.fields.language = app.store.getBy('discussion-languages', 'code', dislang)));
+    } else {
+      const localeComposer = app.forum.attribute('fof-discussion-language.composerLocaleDefault');
+      app.composer.fields.language = localeComposer ? app.store.getBy('discussion-languages', 'code', app.translator.formatter.locale) : '';
+    }
+  });
+
+  DiscussionComposer.prototype.chooseLanguage = function (hide, callback) {
+    app.modal.show(LanguageDiscussionModal, {
+      selected: this.composer.fields.language,
+      hideSubmitButton: hide,
+      onsubmit: (language) => {
+        this.composer.fields.language = language;
+        this.$('textarea').focus();
+
+        if (callback) callback();
+      },
     });
+  };
 
-    DiscussionComposer.prototype.chooseLanguage = function (hide, callback) {
-        app.modal.show(LanguageDiscussionModal, {
-            selected: this.composer.fields.language,
-            hideSubmitButton: hide,
-            onsubmit: (language) => {
-                this.composer.fields.language = language;
-                this.$('textarea').focus();
+  extend(DiscussionComposer.prototype, 'headerItems', function (items) {
+    // Don't add lang item to byobu composers
+    if (this._isByobuComposer) return;
 
-                if (callback) callback();
-            },
-        });
-    };
+    items.add(
+      'language',
+      <a className="DiscussionComposer-changeTags" onclick={this.chooseLanguage.bind(this, true, null)}>
+        <span className={`LanguageLabel ${this.composer.fields.language ? '' : 'none'}`}>
+          {this.composer.fields.language
+            ? Language.component({ language: this.composer.fields.language, uppercase: true })
+            : app.translator.trans('fof-discussion-language.forum.composer_discussion.choose_language_link')}
+        </span>
+      </a>,
+      20
+    );
+  });
 
-    extend(DiscussionComposer.prototype, 'headerItems', function (items) {
-        items.add(
-            'language',
-            <a className="DiscussionComposer-changeTags" onclick={this.chooseLanguage.bind(this, true, null)}>
-                <span className={`LanguageLabel ${this.composer.fields.language ? '' : 'none'}`}>
-                    {this.composer.fields.language
-                        ? Language.component({ language: this.composer.fields.language, uppercase: true })
-                        : app.translator.trans('fof-discussion-language.forum.composer_discussion.choose_language_link')}
-                </span>
-            </a>,
-            20
-        );
-    });
+  override(DiscussionComposer.prototype, 'onsubmit', function (original) {
+    // Don't add lang stuff to byobu composers
+    if (this._isByobuComposer) return original();
 
-    override(DiscussionComposer.prototype, 'onsubmit', function (original) {
-        if (!this.composer.fields.language) return this.chooseLanguage(true, original);
+    if (!this.composer.fields.language) return this.chooseLanguage(true, original);
 
-        original();
-    });
+    original();
+  });
 
-    extend(DiscussionComposer.prototype, 'data', function (data) {
-        data.relationships = data.relationships || {};
+  extend(DiscussionComposer.prototype, 'data', function (data) {
+    // Don't add lang stuff to byobu composers
+    if (this._isByobuComposer) return;
 
-        data.relationships.language = this.composer.fields.language || app.store.all('discussion-languages').sort(sort)[0];
-    });
+    data.relationships = data.relationships || {};
+
+    data.relationships.language = this.composer.fields.language || app.store.all('discussion-languages').sort(sort)[0];
+  });
 };
--- js/src/forum/addLanguageToDiscussionList.js
+++ js/src/forum/addLanguageToDiscussionList.js
@@ -10,71 +10,75 @@ import flag from '../common/utils/flag';
 import LanguageDropdown from './components/LanguageDropdown';
 
 const addLanguage = function (items) {
-    const language = this.attrs.discussion.language();
+  // Ignore PDs from Byobu
+  if (this.attrs.discussion.recipientUsers?.() || this.attrs.discussion.oldRecipientUsers?.()) {
+    return;
+  }
 
-    if (!language) return;
+  const language = this.attrs.discussion.language();
 
-    items.add(
-        'discussion-language',
-        <span>
-            {flag(language) || <i className="fas fa-globe" />}
-            <code>{language.name()}</code>
-        </span>,
-        5
-    );
+  if (!language) return;
+
+  items.add(
+    'discussion-language',
+    <span>
+      {flag(language) || <i className="fas fa-globe" />}
+      <code>{language.name()}</code>
+    </span>,
+    5
+  );
 };
 
 export default () => {
-    extend(DiscussionListItem.prototype, 'infoItems', addLanguage);
-    extend(DiscussionHero.prototype, 'items', addLanguage);
+  extend(DiscussionListItem.prototype, 'infoItems', addLanguage);
+  extend(DiscussionHero.prototype, 'items', addLanguage);
 
-    extend(DiscussionListState.prototype, 'requestParams', function (params) {
-        params.include.push('language');
-        if (app.current.data.routeName === 'byobuPrivate') return;
-        
-        if (params.filter?.tag) params.filter.q = (params.filter.q || '') + ' tag:' + params.filter.tag;
+  extend(DiscussionListState.prototype, 'requestParams', function (params) {
+    if (app.current.data.routeName === 'byobuPrivate') return;
 
-        if (app.forum.attribute('fof-discussion-language.showAnyLangOpt')) {
-            if (app.search.params().language) {
-                params.filter.q = (params.filter.q || '') + ' language:' + app.search.params().language;
-            }
-        } else {
-            params.filter.q =
-                (params.filter.q || '') +
-                ' language:' +
-                (app.search.params().language ? app.search.params().language : app.translator.formatter.locale);
-        }
-    });
+    params.include.push('language');
 
-    extend(GlobalSearchState.prototype, 'stickyParams', (params) => (params.language = m.route.param('language')));
+    if (params.filter?.tag) params.filter.q = (params.filter.q || '') + ' tag:' + params.filter.tag;
 
-    extend(IndexPage.prototype, 'viewItems', function (items) {
-        // Don't add language controls to /private (fof/byobu)
-        if (app.current.data.routeName === 'byobuPrivate') return;
-        
-        let extra, defaultSelected;
-        if (app.forum.attribute('fof-discussion-language.showAnyLangOpt')) {
-            extra = { any: app.translator.trans('fof-discussion-language.forum.index_language.any') };
-            defaultSelected = 'any';
-        } else {
-            defaultSelected = app.translator.formatter.locale;
-        }
+    if (app.forum.attribute('fof-discussion-language.showAnyLangOpt')) {
+      if (app.search.params().language) {
+        params.filter.q = (params.filter.q || '') + ' language:' + app.search.params().language;
+      }
+    } else {
+      params.filter.q =
+        (params.filter.q || '') + ' language:' + (app.search.params().language ? app.search.params().language : app.translator.formatter.locale);
+    }
+  });
 
-        items.add(
-            'language',
-            LanguageDropdown.component({
-                extra,
-                default: defaultSelected,
-                onclick: (key) => {
-                    const params = app.search.params();
+  extend(GlobalSearchState.prototype, 'stickyParams', (params) => (params.language = m.route.param('language')));
 
-                    if (key === defaultSelected) delete params.language;
-                    else params.language = key;
+  extend(IndexPage.prototype, 'viewItems', function (items) {
+    // Don't add language controls to /private (fof/byobu)
+    if (app.current.data.routeName === 'byobuPrivate') return;
 
-                    setRouteWithForcedRefresh(app.route(app.current.get('routeName'), params));
-                },
-                selected: app.search.params().language,
-            })
-        );
-    });
+    let extra, defaultSelected;
+    if (app.forum.attribute('fof-discussion-language.showAnyLangOpt')) {
+      extra = { any: app.translator.trans('fof-discussion-language.forum.index_language.any') };
+      defaultSelected = 'any';
+    } else {
+      defaultSelected = app.translator.formatter.locale;
+    }
+
+    items.add(
+      'language',
+      LanguageDropdown.component({
+        extra,
+        default: defaultSelected,
+        onclick: (key) => {
+          const params = app.search.params();
+
+          if (key === defaultSelected) delete params.language;
+          else params.language = key;
+
+          setRouteWithForcedRefresh(app.route(app.current.get('routeName'), params));
+        },
+        selected: app.search.params().language,
+      })
+    );
+  });
 };
