be433e6fd2f8aa87f399bf9e8312fd0b92747f98
Ian Morland <ian@morland.me>
2021-08-19 18:33:37 +0100

    Revert "Apply fixes from StyleCI"
    This reverts commit 3220c25d04cba53c33878aabeb1c216d124f83c2.

--- extend.php
+++ extend.php
@@ -25,14 +25,14 @@ use FoF\DiscussionLanguage\Api\Serializers\DiscussionLanguageSerializer;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/resources/less/forum.less'),
+        ->js(__DIR__ . '/js/dist/forum.js')
+        ->css(__DIR__ . '/resources/less/forum.less'),
 
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
-        ->css(__DIR__.'/resources/less/admin.less'),
+        ->js(__DIR__ . '/js/dist/admin.js')
+        ->css(__DIR__ . '/resources/less/admin.less'),
 
-    new Extend\Locales(__DIR__.'/resources/locale'),
+    new Extend\Locales(__DIR__ . '/resources/locale'),
 
     (new Extend\Routes('api'))
         ->post('/fof/discussion-language', 'fof.discussion-language.create', Api\Controllers\CreateLanguageController::class)
@@ -89,5 +89,5 @@ return [
         ->serializeToForum('fof-discussion-language.showAnyLangOpt', 'fof-discussion-language.showAnyLangOpt', 'boolVal'),
 
     (new Extend\Filter(DiscussionFilterer::class))
-        ->addFilter(Search\LanguageFilter::class),
+        ->addFilter(Search\LanguageFilter::class)
 ];
